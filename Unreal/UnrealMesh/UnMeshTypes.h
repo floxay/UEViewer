38177801bfae02d1a40f283d4e001c975516c8c7
floxay <57007485+floxay@users.noreply.github.com>
2022-10-02 00:43:28 +0200

    Normal W component.

--- Unreal/UnrealMesh/UnMeshTypes.h
+++ Unreal/UnrealMesh/UnMeshTypes.h
@@ -43,15 +43,18 @@ struct FPackedNormal
 
 		//ugly af, copied the vertex shader glsl code, didnt bother with W component
 		FVector2D v27; // = v30;
+		float f29;
 		v27.X = x;
 		v27.Y = y;
 		if (((int)(v27.X + 0.5)) > 127 && ((int)(v27.Y + 0.5)) > 127)
 		{
+			f29 = -1.f;
 			v27.X = (v27.X + -128.0f) / 127.0f;
 			v27.Y = (v27.Y + -128.0f) / 127.0f;
 		}
 		else
 		{
+			f29 = 1.f;
 			v27.X /= 127.0f;
 			v27.Y /= 127.0f;
 		}
@@ -72,10 +75,15 @@ struct FPackedNormal
 			v28.X = (1.0f + -v33.X) * v34.X;
 			v28.Y = (1.0f + -v33.Y) * v34.Y;
 		}
-		CVec3 v35 = (CVec3&)v28;
-		v35.Normalize();
-		v28 = (FVector&)v35;
-		return *this = v28;
+		CVec3 temp = (CVec3&)v28;
+		temp.Normalize();
+		v28 = (FVector&)temp;
+		FVector4 v35;
+		v35.X = v28.X;
+		v35.Y = v28.Y;
+		v35.Z = v28.Z;
+		v35.W = f29;
+		return *this = v35;
 	}
 
 	FPackedNormal &operator=(const FVector &V)
